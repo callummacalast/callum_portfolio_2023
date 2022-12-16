f4c292987a767fbe902b01bf1d84be9ed06bbd22
Callum Macalast <callummacalast@Callums-MacBook-Pro-2.local>
2022-12-16 11:32:30 +0000

    Changes to site

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -7,9 +7,7 @@ get_header();
         <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
         <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about something? Need more details about me or my tech choice ;)? Let me know.</p>
         <?php echo do_shortcode('[ninja_form id=1]'); ?>
-
     </div>
 </section>
-
 <?php
 get_footer();
