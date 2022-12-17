a30f10482a672692f5dffaf28f46150fbfe7f488
Callum Macalast <callummacalast@Callums-MacBook-Pro-2.local>
2022-12-17 14:15:11 +0000

    post content style

--- single.php
+++ single.php
@@ -12,7 +12,7 @@
 			<div class="px-5 mx-auto sm:p-10 md:p-16 ">
 				<div class="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
 					<img src="<?= get_the_post_thumbnail_url(); ?>" alt="" class="w-full lg:h-60 md:h-52 h-96  object-cover">
-					<div class="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md w-full">
+					<div class="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md w-full post-article">
 						<div class="space-y-2">
 							<a rel="noopener noreferrer" href="#" class="inline-block text-2xl font-semibold sm:text-3xl text-white rounded p-3 bg-blue-500 drop-shadow"><?= the_title(); ?></a>
 							<p class="text-xs ">
