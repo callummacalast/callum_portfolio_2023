c2f6a1a0d71bee99f5b295aa59bbc5ea9a8336e4
Callum Macalast <callummacalast@Callums-MacBook-Pro-2.local>
2022-12-17 14:29:39 +0000

    Contact

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -3,7 +3,7 @@
 get_header();
 ?>
 <section class="bg-white dark:bg-gray-900">
-    <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+    <div class="py-3 lg:py-10 px-4 mx-auto max-w-screen-md">
         <h1 class="text-5xl text-center font-bold">Contact Me<span class="text-blue-600">.</span></h1>
         <p class="mb-8 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about something? Need more details about me or my tech choice ;)? Let me know.</p>
         <?php echo do_shortcode('[ninja_form id=1]'); ?>
